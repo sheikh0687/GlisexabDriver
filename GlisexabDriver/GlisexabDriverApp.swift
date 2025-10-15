fdc0291d7aaa03552fd33e09c3f1a8f1345e4157
sheikh0687 <arbazsheikh99@gmail.com>
2025-10-15 11:47:53 +0530

    Initial Commit

---
+++ GlisexabDriver/GlisexabDriverApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlisexabDriverApp.swift
+//  GlisexabDriver
+//
+//  Created by Techimmense Software Solutions on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct GlisexabDriverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
